0cafbe94d20e885e4ef4b04e76a158191ac2c436
mithun <130681565+nakku123@users.noreply.github.com>
2023-07-16 16:40:37 +0530

    config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "9439609")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "3a64962f1face2fc285d0bb72587b139")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "-1001805053423") 
 
 DB_NAME = os.environ.get("DB_NAME","")     
 
@@ -16,7 +16,7 @@ DB_URL = os.environ.get("DB_URL","")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/6abb0b1f3338755017cff.jpg https://graph.org/file/97afb9f4979168eb90c9a.jpg")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
