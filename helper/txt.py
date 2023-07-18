2a76db444f0ec5cd3f635849c52097f32c16aa7d
mithun <130681565+nakku123@users.noreply.github.com>
2023-07-18 21:02:35 +0530

    txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -69,8 +69,3 @@ Example:- /set_caption 📕 File Name: {filename}
 
 #⚠️ don't remove our credits 🙏😢😢
     DEV_TXT ="https://t.me/englishkaaranmain"
-
- 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 : <a href=https://t.me/memes_mithun>𝗰𝗼𝗻𝘁𝗮𝗰𝘁 𝗺𝗲</a>
-
-  <a href=https://github.com/nakku123>Me Github</a>
-  <a href=https://t.me/memes_mithun>mithun Tg Channel</a>
